076e63ca736920d7564a80e0f8071032b6a13e13
Francisco Sanchez <fsa_sanchez_@hotmail.com>
2024-10-28 21:41:24 -0600

    Update HomeScreen.jsx
    Vamos

--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -68,7 +68,7 @@ export const HomeScreen = ({ setIsLoggedIn }) => {
       <hr className="my-4 divider" />
       <div className="row mt-4">
         <div className="col-12">
-          <h2 className="services-title text-center">Servicios Escolares Para Git actios</h2>
+          <h2 className="services-title text-center">Servicios Escolares Nuevamente</h2>
         </div>
       </div>
       <div className="row mt-4">
