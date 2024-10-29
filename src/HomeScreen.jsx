6835f338e287a82b97aa7a34ef3420c5d69b12be
Francisco Sanchez <fsa_sanchez_@hotmail.com>
2024-10-28 21:53:55 -0600

    Update HomeScreen.jsx
    ibero

--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -68,7 +68,7 @@ export const HomeScreen = ({ setIsLoggedIn }) => {
       <hr className="my-4 divider" />
       <div className="row mt-4">
         <div className="col-12">
-          <h2 className="services-title text-center">Servicios Escolares FSaSanchez</h2>
+          <h2 className="services-title text-center">Servicios Escolares Iberoooonio</h2>
         </div>
       </div>
       <div className="row mt-4">
