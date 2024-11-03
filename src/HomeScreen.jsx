46034658c5a8be812e9230e69cc5fe0b28e75620
Francisco Sanchez <fsa_sanchez_@hotmail.com>
2024-11-03 16:25:04 -0600

    Update HomeScreen.jsx

--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -68,7 +68,7 @@ export const HomeScreen = ({ setIsLoggedIn }) => {
       <hr className="my-4 divider" />
       <div className="row mt-4">
         <div className="col-12">
-          <h2 className="services-title text-center">Hola Teams</h2>
+          <h2 className="services-title text-center">Este es un deploy desde Git Actions y mensaje a teams</h2>
         </div>
       </div>
       <div className="row mt-4">
